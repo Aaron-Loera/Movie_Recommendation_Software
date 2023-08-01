a27b0b60a249eaa2e607e916e8af580401c9611c
Aaron-Loera <loeraaaron776@gmail.com>
2023-08-01 17:24:59 -0500

    Setting up constructor for movie_vertex

--- movie_vertex.py
+++ movie_vertex.py
@@ -1,3 +1,12 @@
 #Creates a movie object that acts as a vertex
 class MovieVertex():
-    pass
+    def __init__(self, name, genre, rating, description):
+        self.name = name
+        self.genre = genre
+        self.rating = rating
+        self.description = description
+        self.edges = {}
+
+
+movie1 = MovieVertex("The Movie", "Horror", 4.5, "A movie where things happend")
+print(movie1.rating)
