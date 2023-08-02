b48ec2b396f78fae6bfdeb81f86383ddb65069d2
Aaron-Loera <loeraaaron776@gmail.com>
2023-08-02 12:32:47 -0500

    Adding add_recommended_movies to movie_vertex

--- movie_vertex.py
+++ movie_vertex.py
@@ -6,7 +6,14 @@ class MovieVertex():
         self.rating = rating
         self.description = description
         self.edges = {}
+    
+    #adds a new edge to the current vertex
+    def add_recommended_movies(self, new_movie):
+        self.edges[new_movie.name] = [new_movie.genre, new_movie.rating, new_movie.description]
 
 
 movie1 = MovieVertex("The Movie", "Horror", 4.5, "A movie where things happend")
-print(movie1.rating)
+movie2 = MovieVertex("The Movie 2", "Action", 3.2, "A movie after the first one")
+movie1.add_recommended_movies(movie2)
+
+print(movie1.edges)
